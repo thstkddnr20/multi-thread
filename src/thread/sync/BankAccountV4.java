ebfa75dc999fe54329cbb956d6b578b078c5b2d4
손상욱 <thstkddnr20@naver.com>
2024-09-27 17:37:33 +0900

    Lock, ReentrantLock

---
+++ src/thread/sync/BankAccountV4.java
@@ -0,0 +1,53 @@
+package thread.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+
+    private int balance;
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        lock.lock(); //ReentrantLock을 이용하여 Lock 걸기
+        try {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false; //return 되면 unlock()이 호출되지 않으므로 finally문에 lock.unlock()을 넣음
+            }
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);
+            balance -= amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        } finally {
+            lock.unlock();
+        }
+
+
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+
+    }
+}
